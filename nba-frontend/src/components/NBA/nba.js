88737817158aa159f7cea44e9185fd4812c9d799
drumondpe <pedrogsd@al.insper.edu.br>
2023-05-16 08:40:40 -0300

    arquivo nba.js e search.js

---
+++ nba-frontend/src/components/NBA/nba.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NBA() {
+  
+      return (<>
+      <div className="appbar">
+        <img className="logo"></img>
+        <h1 className="titulo">NBA APP</h1>
+      </div>
+
+      <div className="corpo">
+        {/* <Search getFilmes={getFilmes} className="BarraEbotao"/> */}
+        <Search className="barra-botao"/>
+        {/* <ResultSearch resultados={lista_detalhes}/> */}
+      </div>
+    </>) 
+  }
---
+++ nba-frontend/src/components/Search/search.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./search.css";
+
+export default function Search(props) {
+
+    function handleKeyDown(event) {
+        if (event.key === "Enter") {
+          event.preventDefault(); // previne o comportamento padrão do formulário
+          props.getFilmes(capitalize(document.getElementsByName("genero")[0].value)); 
+        }
+      }
+
+    return (
+        <form className="barra-botao">
+            <input type="text" name="time" placeholder="What team are you looking for?:" className="barra-pesquisa" onKeyDown={handleKeyDown}/>
+            {/* <button type="submit" onClick={chamaGetFilmes} className="botao"> Search</button> */}
+            <button type="submit" className="botao">Search</button>
+        </form>
+    );
+}
