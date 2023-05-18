67caf4c67bd5d0c1a1a2c157b74c2a1781a6e941
Douglas <douglaspablo2018@gmail.com>
2023-05-18 09:39:27 -0300

    Tela Login

--- nba-frontend/src/App.js
+++ nba-frontend/src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './components/Login/login';
 import NBA from './components/NBA/nba';
 import Card from './components/Card/card';
 
 function App() {
+  const [telaAtual, setTelaAtual] = useState("login");
+
+  const handleChangeTela = () => {
+    setTelaAtual("outraTela");
+  };
+
   return (
     <div className="App">
       <header>
-        <NBA />
+        {telaAtual === "login" ? (
+          <Login onChangeTela={handleChangeTela} />
+        ) : (
+          <NBA />
+        )}
       </header>
     </div>
   );
 }
+
 export default App;
--- nba-frontend/src/components/Login/login.js
+++ nba-frontend/src/components/Login/login.js
@@ -1,24 +1,30 @@
 import React from "react";
 import "./login.css";
 
+export default function Login({ onChangeTela }) {
+  const handleSignIn = () => {
+    // Lógica de autenticação
 
-export default function Login() {
+    // Após o login bem-sucedido, chame a função onChangeTela para atualizar o estado no componente pai
+    onChangeTela();
+  };
+
   return (
     <div className="areaGeral">
-        <div className="form-area">
-            <div className="form-title">
-                <h3>Login</h3>
-            </div>
-            <div className="form-inputs">
-                <label>
-                    <input type="email" id="email" placeholder="Username"/>
-                </label>
-                <label>
-                    <input type="password" id="pass" placeholder="Password" />
-                </label>
-                <button onClick={console.log("clickou")}>Sign In</button>
-            </div>
+      <div className="form-area">
+        <div className="form-title">
+          <h3>Login</h3>
         </div>
-	</div>
+        <div className="form-inputs">
+          <label>
+            <input type="email" id="email" placeholder="Username" />
+          </label>
+          <label>
+            <input type="password" id="pass" placeholder="Password" />
+          </label>
+          <button onClick={handleSignIn}>Sign In</button>
+        </div>
+      </div>
+    </div>
   );
 }
