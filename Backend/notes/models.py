b599b70ec217241e11b00c23940306daa0a25776
pedroivoal <pedroial@al.insper.edu.br>
2023-05-23 08:05:48 -0300

    .

--- Backend/notes/models.py
+++ Backend/notes/models.py
@@ -1,8 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-
-    def __str__(self):
-        return f"{self.id}. {self.title}"
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- Backend/notes/urls.py
+++ Backend/notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('api/notes/<int:note_id>/', views.api_note),
     path('api/notes/', views.api_notes),
+    path('api/token/', views.api_get_token),
+     path('api/users/', views.api_user),
 ]
