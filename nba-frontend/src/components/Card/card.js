a330b39043b05734a1795e3531399360be169681
drumondpe <pedrogsd@al.insper.edu.br>
2023-05-23 09:02:46 -0300

    botão delete iniciado

--- nba-frontend/src/components/Card/card.js
+++ nba-frontend/src/components/Card/card.js
@@ -3,21 +3,29 @@ import "./card.css";
 
 
 export default function Card(props) {
-    return (
+  let botao = null;
+
+  if (window.location.pathname === "/favoritos") {
+    botao = <button className="botao-salvar" onClick={() => console.log("clik")}></button>;
+  } else if (window.location.pathname === "/nba") {
+    botao = <button className="botao-delete" onClick={() => console.log("clik")}></button>;
+  }
+
+  return (
     <div className="card">
-        <div className="card_inner">
-          <div className="card__body card__body--front">
-           <button className="botao-salvar" onClick={console.log("clik")}></button>
-            <img className="img_time" src={props.imagem}/>
-            <h2>{props.title}</h2>
-          </div>
-          <div className="card__body card__body--back">
-            <button className="botao-salvar" onClick={console.log("clik")}></button>
-            <h2>{props.nickname}</h2>
-            <h2>Score: {props.score}</h2>
-            <h2>Country: {props.pais}</h2>
-          </div>
+      <div className="card_inner">
+        <div className="card__body card__body--front">
+          {botao}
+          <img className="img_time" src={props.imagem} alt={props.title} />
+          <h2>{props.title}</h2>
         </div>
+        <div className="card__body card__body--back">
+          {botao}
+          <h2>{props.nickname}</h2>
+          <h2>Score: {props.score}</h2>
+          <h2>Country: {props.pais}</h2>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
